0753dcfd570ab4290354d232e56c1b435ff6609d
felipessalvatore <felipessalvatore@github.com>
2018-03-05 12:27:37 -0300

    add first version of linear regression

--- 2017/slides/install.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install texlive-latex-base
-sudo apt-get install latex-beamer
-sudo apt-get install texlive-latex-extra
-sudo apt-get install texlive-fonts-extra
---
+++ notebooks/util.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_hausing_prices_data(N, verbose=True):
+    """
+    Generates artificial linear data,
+    where x = square meter, y = house price
+
+    :param N: data set size
+    :type N: int
+    :param verbose: param to control print
+    :type verbose: bool
+    :return: design matrix, regression targets
+    :rtype: np.array, np.array
+    """
+    cond = False
+    while not cond:
+        x = np.linspace(90, 1200, N)
+        gamma = np.random.normal(30, 10, x.size)
+        y = 50 * x + gamma * 400
+        x = x.astype("float32")
+        x = x.reshape((x.shape[0], 1))
+        y = y.astype("float32")
+        y = y.reshape((y.shape[0], 1))
+        cond = min(y) > 0
+    xmean, xsdt, xmax, xmin = np.mean(x), np.std(x), np.max(x), np.min(x)
+    ymean, ysdt, ymax, ymin = np.mean(y), np.std(y), np.max(y), np.min(y)
+    if verbose:
+        print("\nX shape = {}".format(x.shape))
+        print("\ny shape = {}".format(y.shape))
+        print("X:\nmean {}, sdt {:.2f}, min {}, max {}".format(xmean,
+                                                               xsdt,
+                                                               xmax,
+                                                               xmin))
+        print("\ny:\nmean {}, sdt {:.2f}, min {}, max {}".format(ymean,
+                                                                 ysdt,
+                                                                 ymax,
+                                                                 ymin))
+    return x, y
+
+
+def plot_points_regression(x,
+                           y,
+                           title,
+                           xlabel,
+                           ylabel,
+                           prediction=None,
+                           legend=False):
+    """
+    Plots the data points and the prediction,
+    if there is one.
+
+    :param x: design matrix
+    :type x: np.array
+    :param y: regression targets
+    :type y: np.array
+    :param title: plot's title
+    :type title: str
+    :param xlabel: x axis label
+    :type xlabel: str
+    :param ylabel: y axis label
+    :type ylabel: str
+    :param prediction: model's prediction
+    :type prediction: np.array
+    :param legend: param to control print legends
+    :type legend: bool
+    """
+    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+    line1, = ax.plot(x, y, 'bo', label='Real data')
+    if prediction is not None:
+        line2, = ax.plot(x, prediction, 'r', label='Predicted data')
+        if legend:
+            plt.legend(handles=[line1, line2], loc=2)
+    ax.set_title(title,
+                 fontsize=20,
+                 fontweight='bold')
+
+    ax.set_xlabel(xlabel, fontsize=20)
+    ax.set_ylabel(ylabel, fontsize=20)
+    plt.show()
