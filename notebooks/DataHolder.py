0fe78102c09b355772c441c2865488f840123026
felipessalvatore <felipessalvatore@github.com>
2018-06-22 15:14:49 -0300

    add DataHolderGentle class

--- notebooks/DataHolder.py
+++ notebooks/DataHolder.py
@@ -1,6 +1,51 @@
 from torch.utils.data import DataLoader
 
 
+
+class GentleLoader:
+    def __init__(self, dataset, batch_size, shuffle):
+        self.dataLoader = DataLoader(dataset=train_dataset,
+                                     batch_size=batch_size,
+                                     shuffle=True)
+    def __iter__(self):
+        return ((batch_X.type(torch.float), batch_y.type(torch.long))
+                 for (batch_X, batch_y) in self.dataLoader)
+
+
+class DataHolderGentle():
+    """
+    Class to store all data using the GentleLoader.
+
+    :param config: hyper params configuration
+    :type config: LRConfig or DFNConfig
+    :param train_dataset: dataset of training data
+    :type train_dataset: torch.utils.data.dataset.TensorDataset
+    :param test_dataset: dataset of test data
+    :type test_dataset: torch.utils.data.dataset.TensorDataset
+    :param valid_dataset: dataset of valid data
+    :type valid_dataset: torch.utils.data.dataset.TensorDataset
+    :param batch_size: batch size for training
+    :type test_batch: batch size for the testing data
+    :param test_batch: int
+    """
+    def __init__(self,
+                 config,
+                 train_dataset,
+                 valid_dataset,
+                 test_dataset,
+                 test_batch=1000):
+        batch_size = config.batch_size
+        self.train_loader = GentleLoader(dataset=train_dataset,
+                                         batch_size=batch_size,
+                                         shuffle=True)
+        self.valid_loader = GentleLoader(dataset=valid_dataset,
+                                         batch_size=batch_size,
+                                         shuffle=True)
+        self.test_loader = GentleLoader(dataset=test_dataset,
+                                        batch_size=test_batch,
+                                        shuffle=True)
+
+
 class DataHolder():
     """
     Class to store all data.
